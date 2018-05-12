07270c8e8a0e8344dd5efcb7a71db4f419870c22
Kristoffer Olsson <kristoffer.olsson@ericsson.com>
2018-05-12 23:25:21 +0200

    Swap package used for generating UUID
    Swapping package 'github.com/satori/go.uuid' for another one,
    'github.com/nu7hatch/gouuid'.
    
    Change-Id: I4f995cfe46e7b50df0329889438abb450d8ad767

--- user-service/proto/user/extensions.go
+++ user-service/proto/user/extensions.go
@@ -1,14 +1,30 @@
 package go_micro_srv_user
 
 import (
+	"log"
+
 	"github.com/jinzhu/gorm"
-	"github.com/satori/go.uuid"
+	// "github.com/satori/go.uuid"
+
+	uuid "github.com/nu7hatch/gouuid"
 )
 
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
-	uuid2, err := uuid.NewV4()
+	// uuid, err := uuid.NewV4()
+	// if err != nil {
+	// 	log.Printf("Something went wrong: %v", err)
+	// 	return err
+	// }
+	// return scope.SetColumn("Id", uuid.String())
+
+	// u1 := uuid.Must(uuid.NewV4())
+	// scope.SetColumn("Id", u1.String())
+	// return nil
+
+	u4, err := uuid.NewV4()
 	if err != nil {
+		log.Printf("Error creating UUID: %v", err)
 		return err
 	}
-	return scope.SetColumn("Id", uuid2.String())
+	return scope.SetColumn("Id", u4.String())
 }
